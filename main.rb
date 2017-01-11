1e2eda34873e166a1f17c005a5c0d0f3fdef68f9
talk2nadji <talk2nadji@hotmail.com>
2017-01-11 11:21:18 +0100

    setup

--- main.rb
+++ main.rb
@@ -1,1 +1,36 @@
 #main.rb
+
+done = false
+
+puts "Hello, welcome to our pizza express delivery"
+
+while not done
+
+  puts "What can i do for you?"
+  puts "1) Order a pizza"
+  puts "2) Nothing"
+  puts "-----"*10
+
+  #get input from user
+  choice = gets.chomp.to_i
+  case choice
+  when 1
+    puts "Let me show you the menu"
+  when 2
+    done = true
+  else
+    puts "I don\'t understand"
+  end
+
+  #say goodby when customer is leaving
+  if done
+    puts "Thank you for your visit! hope to see you again soon."
+    puts "   __"
+    puts " // \"\"--.._"
+    puts "||  (_)  _ \"-._"
+    puts "||    _ (_)    '-."
+    puts "||   (_)   __..-'"
+    puts " \\__..--\"\""
+  end
+
+end
